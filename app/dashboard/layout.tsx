cf1da5cbe576e791fd9746164ea09965a8e80f3e
MRXAyman <elyonkodiko@gmail.com>
2024-11-30 16:39:48 +0100

    Creat Project and add Dashboard page

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import {ReactNode} from "react";
+
+export  default function DashboardLayout({children}: {children: ReactNode}){
+    return(
+        <div className="flex w-full flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
+                <h1>Hello</h1>
+            </header>
+        </div>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+ return (
+     <div>
+         <h1>Hello From the Dashboard rout</h1>
+     </div>
+ )
+}
